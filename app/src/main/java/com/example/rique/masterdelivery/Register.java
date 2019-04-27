d4941160ae71cffc4a5141e18f72035ccafa8f83
hrique35 <hrique35@gmail.com>
2019-04-27 14:51:22 -0300

    Initial commit

---
+++ app/src/main/java/com/example/rique/masterdelivery/Register.java
@@ -0,0 +1,11 @@
+package com.example.rique.masterdelivery;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Register extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
